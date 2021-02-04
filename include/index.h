c489d18699a488338736818d4df943788f225ef2
Nuno Cardoso <nmrcardoso@gmail.com>
2021-02-04 03:16:21 +0000

    Add files via upload

--- include/index.h
+++ include/index.h
@@ -13,25 +13,6 @@ namespace ising{
 
 
 
-
-InlineHostDevice complexd exp_ir(const double a){
-	return complexd(cos(a), sin(a));
-}
-
-
-template<class Real>
-InlineHostDevice complexd GetValue(Real val){ return val; }
-template<> InlineHostDevice complexd GetValue<double>(double val){ return exp_ir(val);}
-template<> InlineHostDevice complexd GetValue<complexd>(complexd val){ return val;}
-
-
-
-InlineHostDevice int indexIdS(const int x[]){
-	int id = x[0];
-	for(int i = 1; i < Dirs()-1; i++)
-		id += x[i] * Offset(i);
-	return id;
-}
 InlineHostDevice int indexId(const int x[]){
 	int id = x[0];
 	for(int i = 1; i < Dirs(); i++)
@@ -42,16 +23,6 @@ InlineHostDevice int indexId(const int x[]){
 InlineHostDevice int GetParity(const int x[]){
 	int oddbit = 0;
 	for(int i = 0; i < Dirs(); i++) oddbit += x[i];
-	return (oddbit & 1);
-}
-InlineHostDevice int GetParityS(const int x[]){
-	int oddbit = 0;
-	for(int i = 0; i < Dirs()-1; i++) oddbit += x[i];
-	return (oddbit & 1);
-}
-InlineHostDevice int GetParity(const int x[], const int maxdir){
-	int oddbit = 0;
-	for(int i = 0; i <= maxdir; i++) oddbit += x[i];
 	return (oddbit & 1);
 }
 
@@ -85,29 +56,6 @@ InlineHostDevice int indexNO_neg(const int id, const int mu, const int lmu, cons
 
 
 
-
-
-InlineHostDevice void indexNOSD(const int id, int x[]){
-	x[2] = (id/(Grid(0) * Grid(1))) % Grid(2);
-	x[1] = (id/Grid(0)) % Grid(1);
-	x[0] = id % Grid(0);
-}
-
-InlineHostDevice int indexNOSD_neg(const int id, const int mu, const int lmu){
-	int x[3];
-	indexNOSD(id, x);
-	x[mu] = (x[mu]+lmu+Grid(mu)) % Grid(mu);	
-	int pos = indexIdS(x);
-	return pos;
-}
-InlineHostDevice int indexNOSD_neg(const int id, const int mu, const int lmu, const int nu, const int lnu){
-	int x[3];
-	indexNOSD(id, x);
-	x[mu] = (x[mu]+lmu+Grid(mu)) % Grid(mu);
-	x[nu] = (x[nu]+lnu+Grid(nu)) % Grid(nu);	
-	int pos = indexIdS(x);
-	return pos;
-}
 
 
 
